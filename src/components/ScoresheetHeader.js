1b3479fccae5061ec874756644f5cec9cba91476
Ben <bmshreeve3@gmail.com>
2024-06-04 15:41:52 +0100

    changes

--- src/components/ScoresheetHeader.js
+++ src/components/ScoresheetHeader.js
@@ -3,8 +3,8 @@ export default function ScoresheetHeader({ gameDetails }) {
     <>
       <div className="flex items-center justify-between  pb-4">
         <img src="/basketballLogo.png" alt="Basketball Logo" className="w-20" />
-        <div className="text-center flex-grow">
-          <h1 className="text-2xl font-extrabold">SCORESHEET</h1>
+        <div className="text-center flex-grow ">
+          <h1 className="text-2xl font-extrabold mr-[70px]">SCORESHEET</h1>
         </div>
       </div>
       <div>
